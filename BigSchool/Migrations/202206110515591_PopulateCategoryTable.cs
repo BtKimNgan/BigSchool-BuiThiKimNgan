fa4c5e4f480ea189ae92dd9921ede6e9889a26ba
btkim <btkim@LAPTOP-2USANSSG>
2022-06-11 13:15:19 +0700

    Add project files.

---
+++ BigSchool/Migrations/202206110515591_PopulateCategoryTable.cs
@@ -0,0 +1,21 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1,'KINH TE')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2,'KINH DOANH')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3,'CONG NGHE THONG TIN')");
+
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
